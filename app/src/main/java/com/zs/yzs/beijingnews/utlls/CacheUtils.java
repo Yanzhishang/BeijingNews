ac932ff668c524bd6a5a04b49271038262f0646a
Yanzhishang <1176377464@qq.com>
2017-04-13 09:12:45 +0800

    1

--- app/src/main/java/com/zs/yzs/beijingnews/utlls/CacheUtils.java
+++ app/src/main/java/com/zs/yzs/beijingnews/utlls/CacheUtils.java
@@ -1,5 +1,10 @@
 package com.zs.yzs.beijingnews.utlls;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.zs.yzs.beijingnews.SplashActivity;
+
 /**
  * Created by YZS on 2017/4/13.
  *
@@ -7,4 +12,16 @@ package com.zs.yzs.beijingnews.utlls;
  */
 
 public class CacheUtils {
+    /**
+     *    得到缓存值
+     *
+     * @param context   上下文
+     * @param key
+     * @return
+     */
+    public static boolean getBoolean(Context context, String key) {
+        SharedPreferences sp = context.getSharedPreferences("yzs", Context.MODE_PRIVATE);
+
+        return sp.getBoolean(key, false);
+    }
 }
